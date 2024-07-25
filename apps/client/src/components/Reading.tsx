82d06dc6fb74ff47dd3875ac5a3d13889caabc5c
wajongwilibrodus <nggobarwajong@gmail.com>
2024-07-25 15:20:26 +0800

    change font weigth and colour

--- apps/client/src/components/Reading.tsx
+++ apps/client/src/components/Reading.tsx
@@ -6,10 +6,10 @@ export const Reading = ({ data}: { data: GetReadings | undefined}) => {
       <h1 className="mt-5 text-center mb-4 text-2xl font-bold tracking-tight leading-none dark:text-white md:text-2xl lg:text-2xl">
         {data?.title}
       </h1>
-      <div className="mx-auto mt-10 px-8  max-w-4xl grid grid-cols-1 gap-6 sm:grid-cols-2">
+      <div className="mx-auto mt-10 px-8  max-w-4xl grid grid-cols-1 gap-6 sm:grid-cols-2 font-medium">
         {data?.verses.map((verse) => (
           <p className="font-zilla text-xl" key={verse.idx}>
-            <span className="mr-10px inline-block align-top text-xs font-bold text-blue-300">
+            <span className="mr-10px inline-block align-top text-xs font-bold text-blue-800 dark:text-blue-300">
               {verse.idx}
             </span>
             {verse.text}
